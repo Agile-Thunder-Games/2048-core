3db75fa8a89ffeebb7f1a3d416b9621bb49ea80a
csyntax <csyntax@outlook.com>
2016-07-20 20:27:05 +0300

    add Tile class

---
+++ Tile.ts
@@ -0,0 +1,40 @@
+interface Position {
+    x: number;
+    y: number;
+}
+
+class Tile {
+    private position: Position;
+    private value: number;
+    private previousPosition: any;
+    private x: number;
+    private y: number;
+    private mergedFrom: any;
+    
+    public constructor(position: Position, value: number) {
+        this.x = position.x;
+        this.y = position.y;
+        this.value = value || 2;
+        this.previousPosition = null;
+        this.mergedFrom = null; // Tracks tiles that merged together
+    }
+
+    public savePosition(): void {
+        this.previousPosition = { x: this.x, y: this.y };
+    }
+
+    public updatePosition(position: Position): void {
+        this.x = position.x;
+        this.y = position.y;
+    }
+    
+    public serialize(): any {
+        return {
+            position: {
+                x: this.x,
+                y: this.y
+            },
+            value: this.value
+        };
+    }
+}
