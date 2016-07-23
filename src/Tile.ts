04c2375d4a2325ce11bddfde3277721fa23be807
csyntax <csyntax@outlook.com>
2016-07-23 22:30:31 +0300

    add getters and setters for Tile class

--- src/Tile.ts
+++ src/Tile.ts
@@ -1,10 +1,10 @@
 class Tile {
-    public position: Point;
-    public value: number;
-    public previousPosition: Point;
-    public x: number;
-    public y: number;
-    public mergedFrom: Tile[];
+    private _position: Point;
+    private _value: number;
+    private _previousPosition: Point;
+    private _x: number;
+    private _y: number;
+    private _mergedFrom: Tile[];
     
     public constructor(position: Point, value: number) {
         this.x = position.x;
@@ -34,5 +34,56 @@ class Tile {
             },
             value: this.value
         };
+    }
+
+    /*
+        Getters and setters
+    */
+    public get position() : Point {
+        return this._position;
+    }
+    
+    public set position(p : Point) {
+        this._position = p;
+    }
+
+    public get value() : number {
+        return this._value;
+    }
+
+    public set value(v : number) {
+        this._value = v;
+    }
+    
+    public get previousPosition() : Point {
+        return this._previousPosition;
+    }
+
+    public set previousPosition(p : Point) {
+        this._previousPosition = p;
+    }
+
+    public get x() : number {
+        return this._x;
+    }
+    
+    public set x(x : number) {
+        this._x = x;
+    }
+    
+    public get y() : number {
+        return this._y;
+    }
+    
+    public set y(y : number) {
+        this._y = y;
+    }
+
+    public get mergedFrom() : Tile[] {
+        return this._mergedFrom;
+    }
+    
+    public set mergedFrom(m : Tile[]) {
+        this._mergedFrom = m;
     }
 }
