3d2968aefe16d7651af1ed6b920ca653a84966a2
Ivan Ivanov <csyntax@outlook.com>
2017-01-25 11:22:47 +0200

    fucking test build

---
+++ built/Direction.js
@@ -0,0 +1,8 @@
+export var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Right"] = 1] = "Right";
+    Direction[Direction["Down"] = 2] = "Down";
+    Direction[Direction["Left"] = 3] = "Left";
+})(Direction || (Direction = {}));
+//# sourceMappingURL=Direction.js.map
---
+++ built/ITraversal.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=ITraversal.js.map
